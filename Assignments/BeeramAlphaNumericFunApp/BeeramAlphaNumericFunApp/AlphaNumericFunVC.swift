a395aa872822340afeef98997020e66a4f8cb651
Harini Beeram <s565877@nwmissouri.edu>
2024-01-31 15:45:49 -0600

    Have some fun app

---
+++ Assignments/BeeramAlphaNumericFunApp/BeeramAlphaNumericFunApp/AlphaNumericFunVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeeramAlphaNumericFunApp
+//
+//  Created by Harini Beeram on 1/31/24.
+//
+
+import UIKit
+
+class AlphaNumericFunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
